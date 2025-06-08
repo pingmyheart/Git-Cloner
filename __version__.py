272715a017317894824ec64a72717e596c4d98d9
GitlabCiCd <antonio.russi.consulting@gmail.com>
2025-06-08 11:53:30 +0200

    [UPDATE] - CI Bugs

--- __version__.py
+++ __version__.py
@@ -1,1 +1,1 @@
-version = "0.0.2.dev0"
+version = "0.0.3.dev0"
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
-from __version='0.0.3.dev0',
+from __version__ import version
 
 setup(
     name="cloner",
-    version='0.0.3.dev0',
+    version=version,
     packages=find_packages(),
     description="Command-Line Interface to manage gitlab or github repositories.",
     author="Antonio Russi",
